9dbfa54a541f61c81312ddcf9a066f578db9819f
DiegoPSP <diego.paulo2022@gmail.com>
2024-02-23 12:24:33 -0300

    Crie um script Python que leia algo do teclado e mostre na tela o seu tipo primitivo e todas as informações possiveis sobre ele.

---
+++ ex003.py
@@ -0,0 +1,5 @@
+valor = input('Digite algo: ')
+print(f"O valor digitado é um numero? {valor.isnumeric()}")
+print(f"O valor digitado é um Alpha? {valor.isalpha()}")
+print(f"O valor digitado é um Alpha Numérico? {valor.isalnum()}")
+
