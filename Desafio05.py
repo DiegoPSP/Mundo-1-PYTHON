43bb4b8d30187e330e2a9947a9c4de23e6e49bc2
DiegoPSP <diego.paulo2022@gmail.com>
2024-02-25 12:05:12 -0300

    Faça um programa que leia um número e mostre na tela o seu antecessor e o sucessor.

---
+++ Desafio05.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número: "))
+antecessor = numero - 1
+sucessor = numero + 1
+print(f'O número digitado foi {numero}, o seu antecessor é {antecessor} e o seu sucessor é {sucessor}')
+print(f'O número digitado foi {numero}, o seu antecessor é {numero-1} e o seu sucessor é {numero+1}')
+#DUAS RESOLUÇÕES
+#1ª com 3 variáveis e mais longa;
+# 2ª com 1 variável e mais curta, utilizando as ferramentas do .format()
