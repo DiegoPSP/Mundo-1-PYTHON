a34c8a03d5a012b92e5c33ed84d911041a50e074
DiegoPSP <diego.paulo2022@gmail.com>
2024-02-25 19:45:41 -0300

    Cálculo de área, usando a largura e altura.

---
+++ Desafio11.py
@@ -0,0 +1,4 @@
+largura = float(input("Digite a largura da parede: "))
+altura = float(input("Digite a altura da parede: "))
+area = largura * altura 
+print(f"Você vai precisar de {area / 2:.2f} litros de tinta para pintar uma parede de {largura:.2f} x {altura:.2f}")
