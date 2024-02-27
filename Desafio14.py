785c858b717157ee0a0038cacd9450a975a48439
DiegoPSP <diego.paulo2022@gmail.com>
2024-02-27 11:09:21 -0300

    Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF.

---
+++ Desafio14.py
@@ -0,0 +1,4 @@
+celsius = float(input("Digite a temperatura em Celsius: "))
+fahrenheit = celsius * 9 / 5 + 32 
+print(f"A temperatura {celsius:.1f}ºC convertida para Fahrenheit é {fahrenheit:.1f}!")
+
