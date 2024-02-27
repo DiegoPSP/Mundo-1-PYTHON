c2a2d673b0a4259d2cf2ee94397e7758201659c9
DiegoPSP <diego.paulo2022@gmail.com>
2024-02-27 10:35:40 -0300

    Adição de formatação melhor e mais organizada

--- Desafio09.py
+++ Desafio09.py
@@ -3,8 +3,9 @@ limite = int(input("Digite até qual ele deve ser multiplicado: "))
 
 def taboada(numero, limite):
     print(f"\nTABELA DE MULTIPLICAÇÃO DE {numero} até {limite}:\n")
+    print('*' * 12)
     for i in range(0, limite + 1):
         resultado = numero * i
-        print(f"{numero} x {i} = {resultado}")
-
+        print(f"{numero} x {i:2} = {resultado:2}")
+    print('*' * 12)
 taboada(numero, limite)
