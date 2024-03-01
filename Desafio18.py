7e2d94b82939a171df7fd3ddab2de02b179af265
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-01 12:17:02 -0300

    Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tanjente desse ângulo.

---
+++ Desafio18.py
@@ -0,0 +1,7 @@
+from math import cos, sin, tan
+angulo = int(input("Digite um ângulo: "))
+seno = sin(angulo)
+cosseno = cos(angulo)
+tanjente = tan(angulo)
+
+print('O ângulo {} possui como seno {} radiano, como cosseno {} radiano e a tangente {} radiano.'.format(angulo, seno, cosseno, tanjente))
