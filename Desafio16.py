77eb1550ab3fdef408f61301c3e55769413f3620
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-01 11:10:03 -0300

    Crie um programa que leia um número real qualquer do teclado e mostre na tela a sua porção inteira.

---
+++ Desafio16.py
@@ -0,0 +1,5 @@
+from math import floor, ceil
+
+numero = float(input("Digite um número: "))
+print(f"A porção inteira desse número é {floor(numero)}") 
+print(f"A porção inteira desse número é {ceil(numero)}") 
