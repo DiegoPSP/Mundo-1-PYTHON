803fec4e7b79644b660c47df4402743fa1128028
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-02 20:01:47 -0300

    Comentários adicionados

--- Desafio19.py
+++ Desafio19.py
@@ -1,3 +1,4 @@
 import random
 alunos = ['Aline', 'Roberta', 'Luana', 'Júlia']
 print(f"A aluna que vai apagar o quadro é a {random.choice(alunos)}!")
+# random.choice faz uma escolha aleatória de acordo com a lista ou parâmetro definido;
