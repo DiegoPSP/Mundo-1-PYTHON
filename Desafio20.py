638c35ac4b3a4a251ff152b4860bd7bab91c3a7f
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-02 20:05:09 -0300

    Comentários adicionados.

--- Desafio20.py
+++ Desafio20.py
@@ -2,3 +2,4 @@ import random
 alunos = ['Sabrina', 'Guilherme', 'Gustavo', 'Joaquim']
 random.shuffle(alunos)
 print(f'\nA sequência de apresentação é: {alunos}\n')
+# random.shuffle Embaralha a lista de forma aleatória;
