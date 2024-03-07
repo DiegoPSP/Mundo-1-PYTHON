2ad55923adaecd87f1bc3bcc8abb15e65ed24f6c
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-07 11:45:57 -0300

    Adição de mais formas de resolver e correções no código.

--- Desafio16.py
+++ Desafio16.py
@@ -1,5 +1,6 @@
 from math import floor, ceil
 
 numero = float(input("Digite um número: "))
-print(f"A porção inteira desse número é {floor(numero)}") 
-print(f"A porção inteira desse número é {ceil(numero)}") 
+print(f"A porção inteira arredondando para baixo desse número é {floor(numero)}") 
+print(f"A porção inteira arredondando para cima é {ceil(numero)}") 
+print(f"O número digitado foi {numero} e a sua porção inteira é {int(numero)}")
