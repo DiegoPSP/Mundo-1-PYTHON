aabde08e403a36b45ea06219c9929cc99da725ca
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-07 11:54:26 -0300

    Adição de mais resolução e correções.

--- Desafio17.py
+++ Desafio17.py
@@ -1,6 +1,9 @@
-from math import sqrt
-cateto_oposto = int(input("Digite o cateto oposto: "))
-cateto_adjacente = int(input("Digite o cateto adjacente: "))
+from math import sqrt, hypot
+cateto_oposto = float(input("Digite o cateto oposto: "))
+cateto_adjacente = float(input("Digite o cateto adjacente: "))
 
-hipotenusa = sqrt(cateto_oposto * cateto_oposto + cateto_adjacente * cateto_adjacente)
-print(f"A hipotenusa com o cateto oposto sendo {cateto_oposto} e o cateto adjacente sendo {cateto_adjacente} é {hipotenusa}!")
+hipotenusa = sqrt(cateto_oposto ** 2 + cateto_adjacente ** 2)
+print(f"A hipotenusa com o cateto oposto sendo {cateto_oposto} e o cateto adjacente sendo {cateto_adjacente} é {hipotenusa:.2f}!")
+
+hipot = hypot(cateto_oposto, cateto_adjacente)
+print(f"A hipotenusa é {hipot:.2f}")
