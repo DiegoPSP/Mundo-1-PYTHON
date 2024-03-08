d520296d866592b93b8da2c29848a541f807798b
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-07 22:47:24 -0300

    Corrigindo a chamada da biblioteca Random e deixando mais leve o código.

--- Desafio19.py
+++ Desafio19.py
@@ -1,4 +1,4 @@
-import random
+from random import choice
 alunos = ['Aline', 'Roberta', 'Luana', 'Júlia']
-print(f"A aluna que vai apagar o quadro é a {random.choice(alunos)}!")
+print(f"A aluna que vai apagar o quadro é a {choice(alunos)}!")
 # random.choice faz uma escolha aleatória de acordo com a lista ou parâmetro definido;
