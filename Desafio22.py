7d8cc3a295ee0634f98b69d7149aa12d83a1d068
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-11 10:56:46 -0300

    Manipulação com o nome inserido do usuário.

---
+++ Desafio22.py
@@ -0,0 +1,7 @@
+nome_completo = str(input('Digite seu nome completo:'))
+espacos = nome_completo.strip()
+primeiro_nome = nome_completo.split()[0]
+print(f"O nome com Capslock é {nome_completo.upper()}!")
+print(f"O nome sem Capslock é {nome_completo.lower()}!")
+print(f"O nome digitado têm {len(espacos)} caracteres(sem os espaços em branco)!")
+print(f"O primeiro nome tem {len(primeiro_nome)} caracteres!")
