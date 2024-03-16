4d0db029290b991ebec8477ec5103d1857d65dfd
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-15 23:35:11 -0300

    Encontrar caracteres em uma string.

---
+++ Desafio26.py
@@ -0,0 +1,4 @@
+frase = str(input("Digite uma frase qualquer: "))
+print(f"A frase digitada possui {frase.lower().count('a')} letras 'A'!")
+print(f"A primeira letra 'A' aparece na posição {frase.lower().find('a')} na sua frase!")
+print(f"A última letra 'A' aparece na posição {frase.lower().rfind('a')}")
