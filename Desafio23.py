8fbed08217073f825539b57273d2457146a2aa15
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-16 00:37:40 -0300

    Corrigindo o código com a aula de como resolver.

--- Desafio23.py
+++ Desafio23.py
@@ -1,7 +1,7 @@
-numero = str(input("Digite um número de 0 a 9999: "))
-unidade = numero[3]
-dezena = numero[2]
-centena = numero [1]
-milhar = numero[0]
+numero = int(input("Digite um número de 0 a 9999: "))
+unidade = numero // 1 % 10
+dezena = numero // 10 % 10
+centena = numero // 100 % 10
+milhar = numero // 1000 % 10
 print(f"\n UNIDADE(S): {unidade}\n DEZENA(S): {dezena}\n CENTENA(S): {centena}\n MILHAR: {milhar}")
 
