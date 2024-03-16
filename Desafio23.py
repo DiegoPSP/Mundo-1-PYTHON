67f6fa888539e2ef85cdce5d6a9f98a663ba338b
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-15 22:46:44 -0300

    Verifica se contém o nome SANTO no nome da cidade.

---
+++ Desafio23.py
@@ -0,0 +1,7 @@
+numero = str(input("Digite um número de 0 a 9999: "))
+unidade = numero[3]
+dezena = numero[2]
+centena = numero [1]
+milhar = numero[0]
+print(f"\n UNIDADE(S): {unidade}\n DEZENA(S): {dezena}\n CENTENA(S): {centena}\n MILHAR: {milhar}")
+
---
+++ Desafio24.py
@@ -0,0 +1,2 @@
+cidade = str(input("Digite o nome da sua cidade: "))
+print(f"A cidade tem o nome SANTO? {"SANTO" in cidade.upper()}!")
