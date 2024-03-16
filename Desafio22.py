20ab23560bff1244cf95b52933ce899938e3d399
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-16 00:26:14 -0300

    Correção do código.

--- Desafio22.py
+++ Desafio22.py
@@ -3,5 +3,5 @@ espacos = nome_completo.strip()
 primeiro_nome = nome_completo.split()[0]
 print(f"O nome com Capslock é {nome_completo.upper()}!")
 print(f"O nome sem Capslock é {nome_completo.lower()}!")
-print(f"O nome digitado têm {len(espacos)} caracteres(sem os espaços em branco)!")
+print(f"O nome digitado têm {(len(espacos) - nome_completo.count(" "))} caracteres(sem os espaços em branco)!")
 print(f"O primeiro nome tem {len(primeiro_nome)} caracteres!")
