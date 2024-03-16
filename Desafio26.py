91da3fcf8663936d0c9580126200557a488a999d
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-16 00:48:35 -0300

    Correção.

--- Desafio26.py
+++ Desafio26.py
@@ -1,4 +1,4 @@
-frase = str(input("Digite uma frase qualquer: "))
+frase = str(input("Digite uma frase qualquer: ")).strip()
 print(f"A frase digitada possui {frase.lower().count('a')} letras 'A'!")
 print(f"A primeira letra 'A' aparece na posição {frase.lower().find('a')} na sua frase!")
 print(f"A última letra 'A' aparece na posição {frase.lower().rfind('a')}")
