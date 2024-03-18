8d39957efaf8f5feb484b1152df67c7f6f6b0f5d
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-18 20:58:58 -0300

    Retirando os prints para testes.

--- Desafio33.py
+++ Desafio33.py
@@ -6,18 +6,14 @@ if maior < meio or maior < menor:
     aux = maior
     maior = meio
     meio = aux
-    print(f"a1- {maior}\n2- {meio}\n3- {menor}")
 
 if maior < menor:
     aux = maior
     maior = menor
     menor = aux
-    print(f"b1- {maior}\n2- {meio}\n3- {menor}")
 
 if meio < menor:
     aux = menor
     menor = meio
     meio = aux
-    print(f"c1- {maior}\n2- {meio}\n3- {menor}")
-
 print(f"O maior número é {maior}, o do meio é {meio} e o menor número é {menor}.")
