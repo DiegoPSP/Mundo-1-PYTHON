13299e5c2e3b00029af0cb20ae1cff7a86f7742e
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-18 16:49:44 -0300

    Uppdate do código.

--- Desafio32.py
+++ Desafio32.py
@@ -1,5 +1,5 @@
 ano = int(input("Digite um ano qualquer: "))
-if ano % 4 == 0:
+if ano % 400 == 0 and ano % 100 == 0:
     print(f"O ano {ano} É um ano bissexto e em fevereiro terá 29 dias!")
 else:
     print(f"O ano {ano} NÃO é um ano bissexto e em fevereiro terá 28 dias!")
