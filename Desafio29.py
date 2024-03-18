e96c66d42194b6963f1864495474446839f2644b
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-18 20:31:49 -0300

    Correção do código e adição d e funções.

--- Desafio29.py
+++ Desafio29.py
@@ -1,7 +1,7 @@
-velocidade = int(input("Digite a velocidade que você estava: "))
+velocidade = float(input("Digite a velocidade que você estava: "))
 if velocidade <= 80:
-    print()
+    print("Tenha um Bom dia! Dirija com segurança!")
 else:
-    print(f"\nVocê estava a {velocidade} e ultrapassou o limite de velocidade permitido!")
+    print(f"\nVocê estava a {velocidade}km/h e ultrapassou o limite de 80 km/h velocidade permitido!")
     velocidade = (velocidade - 80) * 7
-    print(f"Sua multa está no valor de R${velocidade},00.")
+    print(f"Sua multa será no valor de R${velocidade}.")
