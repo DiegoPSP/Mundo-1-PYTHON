f84ac0487957915cd89825bf7e292294112e3b68
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-18 20:14:33 -0300

    Uppdate do código com funcionalidades do professor.

--- Desafio28.py
+++ Desafio28.py
@@ -1,11 +1,13 @@
 from random import randint
+from time import sleep
 
 pense = randint(0, 5)
 print('\nEu pensei em um número, tente adivinhá-lo!')
 chance = int(input("Digite um número de 0 a 5: "))
-
+print("PROCESSANDO...")
+sleep(2)#Faz com que o programa aguarde um tempo antes de continuar a execução; 
 if chance == pense:
-    print(f"\nVocê acertou! Eu pensei em {pense} e você digitou {chance}")
+    print(f"\nVocê acertou! Eu pensei em {pense} e você digitou {chance}!")
 else:
-    print(f"\nVocê errou! Você digitou {chance} e eu pensei em {pense}")
+    print(f"\nVocê errou! Você digitou {chance} e eu pensei em {pense}! HA HA HA")
 print('\nFIM DA BRINCADEIRA!\n') 
