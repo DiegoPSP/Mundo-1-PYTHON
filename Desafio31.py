bbdbbde7afd7c146564d9ec50ea6f244f63fe364
DiegoPSP <diego.paulo2022@gmail.com>
2024-03-18 20:39:16 -0300

    Correção do tipo da variável.

--- Desafio31.py
+++ Desafio31.py
@@ -1,4 +1,4 @@
-distancia = int(input("Digite a distância em Kilometros que você vai fazer: "))
+distancia = float(input("Digite a distância em Kilometros que você vai fazer: "))
 if distancia <= 200:
     distancia = distancia * 0.50
     print(f"Sua passagem ficou no valor de R${distancia}!")
